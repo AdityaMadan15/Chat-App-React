ce481605dc743c3f45e74bfc905c3f332beedfb3
Aditya <adityamadan15@gmail.com>
2025-11-19 12:35:42 +0530

    Fix API URL for production

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || window.location.origin.replace(':3000', ':3001');
+const API_URL = process.env.REACT_APP_API_URL || window.location.origin;
 
 export default API_URL;
